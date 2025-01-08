a081818eb3a5871f5515093e13ad5bbf9204cde0
Manish Sontakke <manyasontakke00@gmail.com>
2025-01-08 13:52:42 +0900

    done for vercel

--- Music_project/settings.py
+++ Music_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^f9w4$&+9r2qq!0k-&ga+0a2$zt%2ady*yr24w!#hcr9q1#lk+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR,'accounts/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
